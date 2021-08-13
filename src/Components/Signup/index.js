682584530694a3f0384bb310d3965b3b1c334e4c
thanhvdt <tthanhvuong11@gmail.com>
2021-08-13 23:12:01 +0700

    Signup_1

--- src/Components/Signup/index.js
+++ src/Components/Signup/index.js
@@ -1,57 +1,83 @@
-import React, {useState} from 'react';
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
 import 'bulma/css/bulma.min.css';
-import { Form } from 'react-bulma-components';
 import './index.css';
 
 const Signup = () => {
-    const [username, setUsername] = useState('');
-    const [birthday, setBirthday] = useState('');
-    const [gender, setGender] = useState('');
-
-
+    
     return (
         <div className = 'form'>
-            <Form.Field>
-                <div className = 'form-style'>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control>
-                        <Form.Input
-                        type = 'text'
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </Form.Control>
+            <div className="field">
+                <label className="label">
+                    Username
+                </label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Text input"/>
                 </div>
-            
-                <div className = 'form-style'>
-                    <Form.Label>D.O.B</Form.Label>
-                    <Form.Control>
-                        <Form.Input
-                        value={birthday}
-                        onChange={(e) => setBirthday(e.target.value)}
-                        />
-                    </Form.Control>
-                </div>
-            
-                <div className = 'form-style'>
-                    <Form.Label>Gender</Form.Label>
-                    <Form.Field kind="group">
-                        <Form.Control>
-                            <Form.Select
-                            value={gender}
-                            onChange={(e) => {
-                                return setGender(e.target.value);
-                            }}
-                            >
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
-                            <option value="other">Other</option>
-                            </Form.Select>
-                        </Form.Control>
-                    </Form.Field>
-                </div>
-            </Form.Field>
+            </div>
 
+            <div className="field">
+                <label className="label">
+                    D.O.B
+                </label>
+                <div className="control">
+                    <input className="input" type="text" placeholder="Text input"/>
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label">Gender</label>
+                <div className="control">
+                    <div className="select">
+                    <select>
+                        <option>Male</option>
+                        <option>Female</option>
+                        <option>Other</option>
+                    </select>
+                    </div>
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label">Interest</label>
+                <div className="control">
+                    <div className="select">
+                    <select>
+                        <option>Male</option>
+                        <option>Female</option>
+                        <option>Other</option>
+                    </select>
+                    </div> 
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label">Max Distance</label>
+                <div className="control">
+                    <input className="slider is-fullwidth" step="1" min="0" max="100" value="50" type="range"/>
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label">Age Range</label>
+                <div className="control">
+                    <input className="slider is-fullwidth" step="1" min="0" max="100" value="50" type="range"/>
+                </div>
+            </div>
+
+            <div className="field">
+                <div className="control">
+                    <button type="button" className="button is-link">CREATE ACCOUNT</button>
+                </div>
+            </div>
+
+            <div className="field">
+                <div className="control">
+                    <button type = "button" className="button is-link is-light">CANCEL</button>
+                </div>
+            </div>
+            
         </div>
        
     )
---
+++ src/Components/Signup/signup.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Signup from './index';
+
+test('renders Header', () => {
+  render(<Signup />);
+  const username = screen.getByText('Username');
+  expect(username).toBeInTheDocument();
+
+  const dateOfBirth = screen.getByText('D.O.B');
+  expect(dateOfBirth).toBeInTheDocument();
+
+  const gender = screen.getByText('Gender');
+  expect(gender).toBeInTheDocument();
+
+  const interest = screen.getByText('Interest');
+  expect(interest).toBeInTheDocument();
+
+  const maxDistance = screen.getByText('Max Distance');
+  expect(maxDistance).toBeInTheDocument();
+
+  const ageRange = screen.getByText('Age Range');
+  expect(ageRange).toBeInTheDocument();
+
+  const createAccount = screen.getByText('CREATE ACCOUNT');
+  expect(createAccount).toBeInTheDocument();
+
+  const cancel = screen.getByText('CANCEL');
+  expect(cancel).toBeInTheDocument();
+});
