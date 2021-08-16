94b54a7f9eeba90ef516bf86c84427b3fdd3c035
handao-everfit <87088870+handao-everfit@users.noreply.github.com>
2021-08-16 10:31:33 -0500

    refractor header and edit styling (#9)
    refractor header and edit styling

---
+++ src/Components/Header/SigninButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SigninButton() {
+  return (
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <button className="sign-in-btn" type="button">
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SigninButton;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,53 +1,17 @@
 import React, { useState } from 'react';
+import 'bulma/css/bulma.min.css';
+import './header.css';
+import HeaderBrand from './HeaderBrand';
+import HeaderMenu from './HeaderMenu';
 
 function Header() {
   const [menuActive, setMenuState] = useState(false);
 
-  const toggleMobileMenu = () => {
-    setMenuState(!menuActive);
-  };
-
   return (
     <div>
-      <nav className="navbar is-black" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="/">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="Logo" />
-          </a>
-
-          <a
-            role="button"
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-            onKeyDown={toggleMobileMenu}
-            onClick={toggleMobileMenu}
-            tabIndex={0}
-          >
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </a>
-        </div>
-
-        <div className={`navbar-menu ${menuActive ? 'is-active' : ''}`}>
-          <div className="navbar-start">
-            <a className="navbar-item">Home</a>
-
-            <a className="navbar-item">FAQs</a>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a className="button is-link">
-                  <strong>Sign in</strong>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <HeaderBrand setMenuState={setMenuState} menuActive={menuActive} />
+        <HeaderMenu menuActive={menuActive} />
       </nav>
     </div>
   );
