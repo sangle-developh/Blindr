5dfb974d12060a028b7f0cd6114f28bb2ac275d7
Han Dao <handao@everfit.io>
2021-08-16 10:14:47 -0500

    delete in file esline disable

--- src/Components/Header/SigninButton.js
+++ src/Components/Header/SigninButton.js
@@ -1,11 +1,12 @@
-/* eslint-disable react/button-has-type */
 import React from 'react';
 
 function SigninButton() {
   return (
     <div className="navbar-end">
       <div className="navbar-item">
-        <button className="sign-in-btn">Sign in</button>
+        <button className="sign-in-btn" type="button">
+          Sign in
+        </button>
       </div>
     </div>
   );
