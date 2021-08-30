b3a3bcd2c520c56a3a4abeff317451f38a887b45
thanhvdt <tthanhvuong11@gmail.com>
2021-08-30 23:57:01 +0700

    quick fix

---
+++ src/Components/Account/account.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Account from './index';
+
+test('renders Header', () => {
+  render(<Account />);
+  const showProfile = screen.getByText('Show my Bindy profile');
+  expect(showProfile).toBeInTheDocument();
+
+  const messageElement = screen.getByText('Message');
+  expect(messageElement).toBeInTheDocument();
+
+  const workPlace = screen.getByText('School/Company');
+  expect(workPlace).toBeInTheDocument();
+});
